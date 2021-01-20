3bf4e1b0481b658de553838f2c4a07cb0fba48c1
David-Bon <davsd333@gmail.com>
2021-01-20 21:09:21 +0200

    "styles +"

--- src/components/ButtonFill.jsx
+++ src/components/ButtonFill.jsx
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 const ButtonFill = ({btnValue, classNumber, color, handleAction}) => {
     return (
         <Button className={classNumber} variant="contained"
-                style={{background: `${color}`, fontSize: "24px", gridArea: `${classNumber}`}}
+                style={{background: `${color}`, fontSize: "24px", gridArea: `${classNumber}`,}}
                 color={`${color === "#D4D4D2" ? "default" : "primary"}`}
                 onClick={() => handleAction(btnValue)}>{btnValue}</Button>
     )
